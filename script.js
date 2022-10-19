035aebd498ef5717ea1f9eefea3b91e73be9f41c
pravirchugh <71580312+pravirchugh@users.noreply.github.com>
2022-10-19 12:37:12 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+function addTodo(){
+
+
+
+    var s = document.getElementById("text_field").value;
+    document.getElementById("text_field").value = "";
+
+    if(s != ""){
+        
+        // Show the new todo on the screen
+
+        var ol = document.getElementById("main_list");
+        var li = document.createElement("li");
+
+        li.appendChild(document.createTextNode(s));
+
+        ol.appendChild(li);
+    }
+}
+
+function removeLast(){
+    var ol = document.getElementById("main_list");
+
+    ol.removeChild(ol.lastChild);
+}
+
+function removeAll(){
+
+    document.getElementById("text_field").value = "";
+
+    document.getElementById("main_list").innerHTML = "";
+
+}
+
